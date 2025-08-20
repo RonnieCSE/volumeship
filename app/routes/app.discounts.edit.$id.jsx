e90539d83f9f0daea97b89069844f63c8892ef12
Ronnie <ronniearea@gmail.com>
2025-08-20 18:49:00 +0600

    trying to fetch metaobject on edit mode

--- app/routes/app.discounts.edit.$id.jsx
+++ app/routes/app.discounts.edit.$id.jsx
@@ -1,30 +1,27 @@
 
 import {json} from "@remix-run/node";
 import {authenticateExtra} from "../config/shopify.js";
-import {VolumeShipModel} from "../models/volumeship.model";
+import {VolumeShipModel} from "../models/volumeship.model.js"
 import {useLoaderData} from "@remix-run/react";
 import { DiscountForm } from "../components/discounts/discountForm.jsx";
 
 
 export const loader = async ({params, request}) => {
-
   const {admin, metaobject} = await authenticateExtra(request)
-
   const discountId = `gid://shopify/Metaobject/${params.id}`;
-    console.log(discountId)
 
-  return json({})
   try {
-    // const discountData = await metaobject.find(VolumeDiscountModel, discountId);
+    const discountData = await metaobject.find(VolumeShipModel, discountId);
 
-    // const parsedData = {
-    //   ...discountData,
-    //   products: JSON.parse(discountData.products),
-    //   discountValues: JSON.parse(discountData.discountValues),
-    //   combinesWith: JSON.parse(discountData.combinesWith),
-    //   isActive: discountData.isActive === 'true',
-    // };
-    // return json(parsedData);
+    const parsedData = {
+      ...discountData,
+      products: discountData.products,
+      discountValues: discountData.discountValues,
+      combinesWith: discountData.combinesWith,
+      isActive: discountData.isActive,
+    };
+
+    return json(parsedData);
   } catch (error) {
     console.error("Error loading discount data:", error);
     return json({ error: "Failed to load discount data" }, { status: 500 });
@@ -33,13 +30,16 @@ export const loader = async ({params, request}) => {
 }
 
 export async function action({request}) {
+  const { admin, metaobject } = await authenticateExtra(request);
+  let formData = await request.json();
 
+  console.log('>>>> formData >>>>')
+  console.log(formData)
 }
 export default function discountEditPage() {
   const loaderData = useLoaderData();
   if (loaderData.error) {
     return <div>Error: {loaderData.error}</div>;
   }
-
   return <DiscountForm  isEditing={true} />;
 }
